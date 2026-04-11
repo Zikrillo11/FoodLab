42424db334cb7b1c4bd1543de56e1a75d2de0c2a
Zikirillo11 <zikrilloyoldashev11@gmail.com>
2026-04-11 16:44:54 +0500

    Dto togirlandi

---
+++ FoodLab.Shared/DTOs/Base/BaseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace FoodLab.Shared.DTOs.Base;
+
+public class BaseDto
+{
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime? UpdatedAt { get; set; }
+    public bool IsDeleted { get; set; } = false;
+    public long CreatedBy { get; set; }
+    public long UpdatedBy { get; set; }
+}
